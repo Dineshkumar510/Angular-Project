0e096e751b1f00a2af4821803972ac333faf8ce7
Dineshkumar510 <dk6089567@gmail.com>
2021-10-25 23:11:55 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path'); 
-const port = 3000;
+const cors = require('cors');
+const port = 5000;
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,12 +13,22 @@ app.get('/', (req, res)=> {
     res.send(`Listening to Demo Project at port ${port}`);
 })
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
 app.get('/download', (req, res)=>{
     const file = 'Client/src/assets/pdf/sample.pdf';
     res.download(file);
     res.send("Download File");
 })
 
+
+app.use(cors());
 
 app.listen(port, ()=> {
     console.log(`Now Listening on Port ${port}`);
